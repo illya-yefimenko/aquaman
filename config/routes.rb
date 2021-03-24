56f1bbadf55602a5fa58872e99ea3fb9de71fc71
Illya Yefimenko <illyayefimenko@gmail.com>
2021-03-24 15:07:09 -0400

    updated routes to use "resources" for devices

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'devices/show'
-  get 'devices/index'
-  get 'devices/create'
-  get 'devices/edit'
-  get 'devices/update'
-  get 'devices/destroy'
-  get 'water_level/shouldfill'
-  get 'water_level/index'
-  get 'floats/read'
-  get 'floats/write'
+  
+  resources :devices
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
