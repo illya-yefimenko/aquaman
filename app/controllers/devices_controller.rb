1956c7bbd9fe1796b99db7598956c5812581b362
Illya Yefimenko <illyayefimenko@gmail.com>
2021-03-24 16:07:07 -0400

    filled out Device controller

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,19 +1,49 @@
 class DevicesController < ApplicationController
   def show
+    @device = Device.find(params[:id])
   end
 
   def index
+    @devices = Device.all
+  end
+
+  def new
+    @device = Device.new
   end
 
   def create
+    @device = Device.new(device_params)
+
+    if @device.save
+      redirect_to @device
+    else
+      render :new
+    end
   end
 
   def edit
+    @device = Device.find(params[:id])
   end
 
   def update
+    @device = Device.find(params[:id])
+    if @device.update(device_params)
+      redirect_to @device
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @device = Device.find(params[:id])
+    @device.destroy
+
+    redirect_to root_path
+  end
+
+
+  private
+  def device_params
+    params.require(:device).permit(:name);
   end
 end
