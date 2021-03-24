5ba0fc613d840a77db660b533af447004ba4d1e7
Illya Yefimenko <illyayefimenko@gmail.com>
2021-03-24 19:23:54 -0400

    .idea iml

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,19 +1,19 @@
-class Property
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  
-  field :name, type: String
-  field :unit, type: String
-  field :type, type: Integer
-  field :io_direction, type: Integer
-  
-  belongs_to :device
-
-  enum type: [:number, :boolean]
-  enum io_direction: [:input, :output]
-
-  validates :name, presence: true
-  validates :unit, presence: true
-  validates :type, inclusion: { in: types.keys }
-  validates :io_direction, inclusion: { in: io_directions.keys }
-end
+# class Property
+#   include Mongoid::Document
+#   include Mongoid::Timestamps
+#
+#   field :name, type: String
+#   field :unit, type: String
+#   field :type, type: Integer
+#   field :io_direction, type: Integer
+#
+#   enum type: [:number, :boolean]
+#   enum io_direction: [:input, :output]
+#
+#   validates :name, presence: true
+#   validates :unit, presence: true
+#   validates :type, inclusion: { in: types.keys }
+#   validates :io_direction, inclusion: { in: io_directions.keys }
+#
+#   belongs_to :device
+# end
