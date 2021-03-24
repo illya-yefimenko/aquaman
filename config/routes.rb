a777c4685cb04c0922f1da3df6e1443c8bc58cda
Illya Yefimenko <illyayefimenko@gmail.com>
2021-03-24 15:09:04 -0400

    generated resources controller for "Properties"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
-  resources :devices
+
+  resources :devices do
+    resources :properties
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
