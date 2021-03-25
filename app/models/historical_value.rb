cd9794f4c4d35c44880d9d2290c699a4cf81a9f7
Illya Yefimenko <illyayefimenko@gmail.com>
2021-03-24 22:57:22 -0400

    created historical value model

---
+++ app/models/historical_value.rb
@@ -0,0 +1,4 @@
+class HistoricalValue
+  include Mongoid::Document
+  include Mongoid::Timestamps
+end
---
+++ test/models/historical_value_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HistoricalValueTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
