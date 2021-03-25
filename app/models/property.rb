9b7931a1ee9443dabab0e29168ecbe05c0bf7b44
Illya Yefimenko <illyayefimenko@gmail.com>
2021-03-24 22:48:08 -0400

    set symbol fields to use StringifiedSymbol type and added functions to access symbol arrays and get proper typed "value"

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,13 +7,31 @@ class Property
   field :unit, type: String
     validates :unit, presence: true
 
-  field :value_type
+  field :value_type, type: StringifiedSymbol
     VALUE_TYPES = [:number, :boolean]
     validates :value_type, inclusion: { in: VALUE_TYPES }
 
-  field :io_direction
+  field :io_direction, type: StringifiedSymbol
     IO_DIRECTIONS = [:input, :output]
-    validates :io_direction, inclusion: {in: IO_DIRECTIONS }
+    validates :io_direction, inclusion: { in: IO_DIRECTIONS }
+
+  field :value
 
   belongs_to :device
+
+  public
+  def self.getValueTypes
+    VALUE_TYPES
+  end
+  def self.getIODirections
+    IO_DIRECTIONS
+  end
+  def getValue
+    case :value_type
+    when :number
+      :value
+    when :boolean
+      :value ? true : false
+    end
+  end
 end
