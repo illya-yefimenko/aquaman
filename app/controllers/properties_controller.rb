051f1de7826171fb1ed355d803070a65ff75d655
Illya Yefimenko <illyayefimenko@gmail.com>
2021-03-24 22:46:03 -0400

    added create and destroy functions to properties controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,11 +1,8 @@
 class PropertiesController < ApplicationController
-  def show
-  end
-
-  def index
-  end
-
   def create
+    @device = Device.find(params[:device_id])
+    @property = @device.properties.create(property_params)
+    redirect_to device_path(@device)
   end
 
   def edit
@@ -15,5 +12,15 @@ class PropertiesController < ApplicationController
   end
 
   def destroy
+    @device = Device.find(params[:device_id])
+    @property = @device.properties.find(params[:id])
+    @property.destroy
+
+    redirect_to device_path(@device)
+  end
+
+  private
+  def property_params
+    params.require(:property).permit(:name, :unit, :value_type, :io_direction)
   end
 end
