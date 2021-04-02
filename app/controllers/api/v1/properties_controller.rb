8a4288d2ba5b6e67d4ac3f703b3b3f908b68735e
Illya <illyayefimenko@gmail.com>
2021-04-02 18:53:54 -0400

    added handling for show and fixed find in update on properties controller

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,11 +1,25 @@
 class Api::V1::PropertiesController < ::Api::ApiController
+
   def update
-    @property = Property.find params[:id]
+    @property = Property.find(params[:property_id])
 
     if @property
       @property.update! property_params
     else
       render json: { error: 'Not found' }, status: :not_found
+    end
+  end
+  def show
+    @property = Property.find(params[:id]);
+
+    if @property
+      render json: { value: @property.value }
+    end
+  end
+
+  def fix_types
+    if params[:property][:value]
+      params[:property][:value] = params[:property][:value].to_i
     end
   end
 
